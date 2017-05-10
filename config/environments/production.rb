67b9d9724df7eed6eef8f3c28fed6944ca38dc47
st118173 <st118173@ait.asia>
2017-05-10 19:01:16 +0700

    gvhn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,25 +12,14 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.smtp_settings = {
-      :address => "smtp.bizmail.yahoo.com",
+      :address => "smtp.mail.yahoo.com",
       :port => 587,
       :domain => "yahoo.com",
       user_name: 'registration_facebookmail@yahoo.com',
       password:  'pzljvmwuiavbwzyu',
       :enable_starttls_auto => true,
-      :authentication => :plain
-  }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.smtp_settings = {
-      :address => "smtp.gmail.com",
-      :port => 587,
-      :domain => "gmail.com",
-      user_name: 'jntu784@gmail.com',
-      password:  'bvvpvybuvsrecqlj',
-      :authentication => 'plain',
-      :enable_starttls_auto => true
+      :authentication => :plain,
+      :RequestTimeout => 30
   }
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false

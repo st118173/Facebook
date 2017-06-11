c71cff71b7cab6c90794d4e764b723f10c0c88a1
st118173 <st118173@ait.asia>
2017-06-11 19:09:47 +0700

    ss

--- app/controllers/fbs_controller.rb
+++ app/controllers/fbs_controller.rb
@@ -61,7 +61,7 @@ class FbsController < ApplicationController
   def destroy
     @fb.destroy
     respond_to do |format|
-      format.html { redirect_to fbs_path, notice: 'Client Facebook Hacked record is deleted.' }
+      format.html {   flash[:notice]= 'Client Facebook Hacked record is deleted.' }
       format.json { head :no_content }
     end
   end

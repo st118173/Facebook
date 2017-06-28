755126a948d2ab0c6434128d90b0c257145bd1af
st118173 <st118173@ait.asia>
2017-06-28 20:26:50 +0700

    kjn

--- config/deploy.rb
+++ config/deploy.rb
@@ -65,7 +65,7 @@
 
 
 # Change these
-server '34.212.136.221', roles: [:web, :app, :db], primary: true
+server 'ec2-34-212-136-221.us-west-2.compute.amazonaws.com', roles: [:web, :app, :db], primary: true
 
 set :repo_url,        "git@github.com:st118173/Facebook.git"
 set :application,     'Facebook'
@@ -84,7 +84,7 @@ set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
 set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/Facebook_jntu.pem) }
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord

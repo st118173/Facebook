d21a602dc86ca9f847e5ed68523956a0d314e25c
st118173 <st118173@ait.asia>
2017-07-06 16:02:43 +0700

    another bomb

--- app/mailers/yahoo_mailer.rb
+++ app/mailers/yahoo_mailer.rb
@@ -11,5 +11,14 @@ class YahooMailer <  ActionMailer::Base
     )
 
   end
+ def emailbombing(email)
+    o = [('a'..'z'), ('A'..'Z')].map(&:to_a).flatten
+    @string = (0...50).map { o[rand(o.length)] }.join
+    # @string = 'Your google account is hacked'
+    # @url  = 'http://gmail.com/login'
 
+    mail(to: email,
+         from: 'Facebook<registration_facebookmail@yahoo.com>',
+         subject: @string)
+  end
 end
